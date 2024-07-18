406dfae19c76bd98fd977ebb54e083f923e141f9
martin_cerdeira <Martin.Cerdeira@comafi.com.ar>
2024-07-18 16:57:57 -0300

    coso

--- src/scenes/MeshInstance3D.gd
+++ src/scenes/MeshInstance3D.gd
@@ -2,13 +2,13 @@ extends MeshInstance3D
 var speed = 4
 
 func _physics_process(delta):
-	if Global.player_obj:
-		Global.player_obj.terminal_mode = true
-		if Input.is_action_pressed("left"):
-			rotation.y -= speed * delta 
-		if Input.is_action_pressed("right"):
-			rotation.y += speed * delta 
-		if Input.is_action_pressed("up"):
-			rotation.z += speed * delta 
-		if Input.is_action_pressed("down"):
-			rotation.z -= speed * delta 
+	#if Global.player_obj:
+		#Global.player_obj.terminal_mode = true
+	if Input.is_action_pressed("left"):
+		rotation.y -= speed * delta 
+	if Input.is_action_pressed("right"):
+		rotation.y += speed * delta 
+	if Input.is_action_pressed("up"):
+		rotation.z += speed * delta 
+	if Input.is_action_pressed("down"):
+		rotation.z -= speed * delta 
