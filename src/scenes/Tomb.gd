55e332ead674e9885c99002099665e56896e9405
martin_cerdeira <Martin.Cerdeira@comafi.com.ar>
2024-07-18 15:39:58 -0300

    cosos

--- src/scenes/Tomb.gd
+++ src/scenes/Tomb.gd
@@ -1,4 +1,53 @@
-extends Node2D
+extends Area2D
+var active = false
+var opened = false
+var player = null
+var delay_camera = 0.2
+var has_artifact = false
+var itemfound = load("res://scenes/Item3D.tscn")
 
 func _ready():
+	$display.visible = false
 	$sprite.frame = randi() % 4
+
+func _physics_process(delta):
+	if !active and opened:
+		if player:
+			if delay_camera > 0:
+				delay_camera -= 1 * delta
+				if delay_camera <= 0:
+					player.dont_camera = false
+	
+	if active and !opened:
+		if Input.is_action_just_pressed("up"):
+			opened = true
+			active = false
+			Global.emit(global_position, 5)
+			get_item()
+			
+func get_item():
+	if has_artifact:
+		var p = itemfound.instantiate()
+		var parent = get_parent()
+		parent.add_child(p)
+		$display.visible = false
+	else:
+		$display/back/lbl_item.text = "NOTHING FOUND..."
+		$display/back/sprite.visible = false
+		
+
+func _on_body_entered(body):
+	if !opened and body.is_in_group("players") and !body.is_in_group("prisoners"):
+		body.dont_camera = true
+		$display.visible = true
+		var center = Global.shaker_obj.camera.get_screen_center_position()
+		$display.global_position = center
+		active = true
+		player = body
+
+func _on_body_exited(body):
+	if body.is_in_group("players") and !body.is_in_group("prisoners"):
+		body.dont_camera = false
+		$display.visible = false
+		active = false
+		body.dont_camera = false
