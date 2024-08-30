c3f35fdbf2c2fdba175bbcdb369ac58b02c37fe6
martin_cerdeira <Martin.Cerdeira@comafi.com.ar>
2024-08-30 17:19:35 -0300

    diseñar sistema de skins para escaleras

--- src/scenes/stair.gd
+++ src/scenes/stair.gd
@@ -1,7 +1,20 @@
 extends Area2D
 @export var plant_stair = false
+@export var tilemap : TileMap
+var stair = 0
+var rope = 1
+var vine = 3
 
 func _ready():
+	if tilemap:
+		var transf = Global.pick_random([stair, rope, vine])
+		
+		var mult = scale.y
+		var pos_calc = Vector2(position.x, position.y - (32 * mult))
+		var pos_in_tile = tilemap.local_to_map(pos_calc)
+		for i in range(pos_in_tile.y, pos_in_tile.y + mult):
+			tilemap.set_cell(0, Vector2(pos_in_tile.x, i), transf, Vector2(0, 0))
+		
 	add_to_group("stairs")
 	if plant_stair:
 		add_to_group("plant_stair")
