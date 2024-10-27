2f0c85d66be2887ea85cdf50738a35d4b0f18cc3
Martin Cerdeira <martincerdeira@gmail.com>
2024-10-26 23:36:45 -0300

    epilepsia

--- src/scenes/boss_enemy_2.gd
+++ src/scenes/boss_enemy_2.gd
@@ -3,13 +3,23 @@ var active = false
 var dead = false
 var TOTAL_LIFE = 5.0
 var LIFE = TOTAL_LIFE
+var idx = 0
+@export var pos : Array[Marker2D]
+@export var Anim : AnimationPlayer
 
 func _ready():
 	Global.play_sound(Global.GhostBossLaughSFX)
-
+	
+func position():
+	global_position = pos[idx].global_position
+	idx += 1
+	if idx > pos.size() - 1:
+		idx = 0
+	
 func activate():
 	Ambience.stop()
-	Music.play(Global.BossThemeGhost)
+	Music.stop()
+	Anim.play("new_animation")
 	visible = true
 	active = true
 	Global.boss_bar.showme()
