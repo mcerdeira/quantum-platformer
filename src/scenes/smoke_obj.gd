b5d034b8a41e00b0bef8243259701d4a79995fc7
Martin Cerdeira <martincerdeira@gmail.com>
2024-11-26 00:47:12 -0300

    Bomba de humo

--- src/scenes/smoke_obj.gd
+++ src/scenes/smoke_obj.gd
@@ -1,48 +1,80 @@
 extends Area2D
 var ttl = 10
-var smoke_obj = load("res://scenes/smoke_obj.tscn")
-var iteration = 0
-var go_up = true
-var go_down = true
-var go_left = true
-var go_right = true
+var smoke_obj = load("res://scenes/smokito.tscn")
+var idx = 0
+var done = false
+var start_killing = false
+var childs_ref = []
+var vector_array: Array = [
+	# Centro
+		Vector2(0, 0),
+		
+		# Anillo 1
+		Vector2(0, -32), Vector2(-32, 0), Vector2(32, 0), Vector2(0, 32),
+		
+		# Anillo 2
+		Vector2(-64, -32), Vector2(-32, -32), Vector2(32, -32), Vector2(64, -32),
+		Vector2(-64, 32), Vector2(-32, 32), Vector2(32, 32), Vector2(64, 32),
+		
+		# Anillo 3
+		Vector2(-96, -64), Vector2(-64, -64), Vector2(-32, -64), Vector2(0, -64), 
+		Vector2(32, -64), Vector2(64, -64), Vector2(96, -64),
+		Vector2(-96, 64), Vector2(-64, 64), Vector2(-32, 64), Vector2(0, 64), 
+		Vector2(32, 64), Vector2(64, 64), Vector2(96, 64),
+		
+		# Anillo 4
+		Vector2(-128, -32), Vector2(-96, -32), Vector2(-64, -32), Vector2(-32, -32),
+		Vector2(32, -32), Vector2(64, -32), Vector2(96, -32), Vector2(128, -32),
+		Vector2(-128, 32), Vector2(-96, 32), Vector2(-64, 32), Vector2(-32, 32),
+		Vector2(32, 32), Vector2(64, 32), Vector2(96, 32), Vector2(128, 32),
+		
+		# Anillo 5
+		Vector2(-160, 0), Vector2(-128, 0), Vector2(-96, 0), Vector2(-64, 0),
+		Vector2(-32, 0), Vector2(32, 0), Vector2(64, 0), Vector2(96, 0),
+		Vector2(128, 0), Vector2(160, 0),
+		
+		# Anillo 6
+		Vector2(-32, -128), Vector2(0, -128), Vector2(32, -128),
+		Vector2(-64, -96), Vector2(-32, -96), Vector2(0, -96), Vector2(32, -96), Vector2(64, -96),
+		Vector2(-64, 96), Vector2(-32, 96), Vector2(0, 96), Vector2(32, 96), Vector2(64, 96),
+		Vector2(-32, 128), Vector2(0, 128), Vector2(32, 128),
+		Vector2(0, 160),
+		Vector2(0, -160)
+]
 
 func _ready():
 	add_to_group("smoke_obj")
-	$AnimationPlayer.play("new_animation")
-	$sprite.play()
 	
 func _physics_process(delta):
-	ttl -= 1 * delta
-	if ttl <= 0:
-		Global.emit(global_position, 10)
-		queue_free()
-		return
-	
-func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	if body is TileMap:
-		var coords = body.get_coords_for_body_rid(body_rid)
-		if body.get_cell_source_id(0, coords) == 0:#SMOKE
-			$Timer.stop()
+	if start_killing:
+		ttl -= 1 * delta
+		if ttl <= 0:
+			childs_ref.shuffle()
+			var to_kill = childs_ref.pop_front()
+			Global.emit(to_kill.global_position, 5)
+			to_kill.queue_free()
+			ttl = 0.3
+			if childs_ref.size() <= 0:
+				Global.emit(to_kill.global_position, 10)
+				queue_free()
+	else:
+		if done:
+			ttl -= 1 * delta
+			if ttl <= 0:
+				ttl = 0.5
+				start_killing = true
 
 func _on_timer_timeout():
-	if iteration <= 4:
+	if idx < vector_array.size():
+		create_smoke(vector_array[idx])
+		idx += 1
+	else:
 		$Timer.stop()
-		if $position_free_up.free:
-			create_smoke_object(Vector2(global_position.x, global_position.y - 32))
-			
-		if $position_free_down.free:
-			create_smoke_object(Vector2(global_position.x, global_position.y + 32))
-			
-		if $position_free_left.free:
-			create_smoke_object(Vector2(global_position.x - 32, global_position.y))
-			
-		if $position_free_right.free:
-			create_smoke_object(Vector2(global_position.x + 32, global_position.y))
+		done = true
 
-func create_smoke_object(pos):
+func create_smoke(pos):
 	var smoke_o = smoke_obj.instantiate()
-	smoke_o.global_position = pos
-	smoke_o.iteration = iteration + 1
-	get_parent().add_child(smoke_o)
+	smoke_o.position = pos
+	add_child(smoke_o)
 	Global.emit(global_position, 10)
+	childs_ref.append(smoke_o)
