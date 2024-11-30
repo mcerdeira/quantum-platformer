572ce19798f75512ce372c9a26cd1fbb104fbba3
Martin Cerdeira <martincerdeira@gmail.com>
2024-11-29 21:02:59 -0300

    waterfall

--- src/scenes/waterfall.gd
+++ src/scenes/waterfall.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 func _ready():
 	if Global.TerminalNumber == Global.TerminalsEnum.MERMAID:
-		modulate.a = Global.pick_random([0.43, 0.1, 0.7])
+		var val = Global.pick_random([0.1, 0.1, 0.1, 1])
+		modulate.a = val 
+		if val == 1:
+			z_index = 4095
+		
 	else:
 		queue_free()
