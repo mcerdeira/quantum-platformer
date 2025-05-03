5cdf8fc2db1becb6d7288074b22a4f95bf79488d
Martin Cerdeira <martincerdeira@gmail.com>
2025-05-03 00:17:23 -0300

    cososos

--- src/scenes/levels/dragon/pet_persecution.gd
+++ src/scenes/levels/dragon/pet_persecution.gd
@@ -1,7 +1,22 @@
 extends Area2D
+var following = false
+var follower = null
+var speed = 100#213
+
+func _ready() -> void:
+	visible = false
+	follower = get_parent()
+
+func _physics_process(delta: float) -> void:
+	if following:
+		follower.progress += speed * delta
 
 func _on_activator_body_entered(body: Node2D) -> void:
-	if $"../boss".persecution:
-		if !visible and body and body.is_in_group("players"):
-			visible = true
-			$"../boss".visible = false
+	if $"../../../boss".persecution:
+		if body and body.is_in_group("players"):
+			if !visible:
+				following = true
+				visible = true
+				$"../../../boss".visible = false
+			else:
+				body.kill()
